9bf2053ea0e7bdf4fef9bbb70e04c316516a4d04
Zach Padovano <zacharyaz10@gmail.com>
2023-04-12 09:57:39 -0700

    First commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 3);
+            Console.WriteLine("Can you guess what my favorite number is?");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("Nice job!");
+            }
+           else if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine("Too high! Guess again!");
+            }
         }
     }
 }
+
+
